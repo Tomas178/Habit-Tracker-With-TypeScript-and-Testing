15398a2d280e51304ab90f5e5fd71dd49fede081
Tomas178 <tomaspetronis77@gmail.com>
2025-07-03 23:28:04 +0300

    Added DateButton tests

---
+++ src/__tests__/DateButton.spec.ts
@@ -0,0 +1,42 @@
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import { createRouter, createWebHistory } from 'vue-router';
+import DateButton from '@/components/DateNavigation//DateButton.vue';
+import type DateForNavigation from '@/models/DateForNavigation';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/day/:date',
+      name: 'DateRoute',
+      component: { template: '<div></div>' }, // dummy for test
+    },
+  ],
+});
+
+const someDate: DateForNavigation = {
+  dateDay: 15,
+  nameDay: 'TU',
+  fullDate: '2025-07-15',
+};
+
+describe('DateButton', () => {
+  it('Renders properly', async () => {
+    router.push({ name: 'DateRoute', params: { date: someDate.fullDate } });
+    await router.isReady();
+
+    const button = mount(DateButton, {
+      global: {
+        plugins: [router],
+      },
+      props: {
+        day: someDate,
+        disabled: false,
+      },
+    });
+
+    expect(button.exists()).toBe(true);
+    expect(button.text()).toContain(`${someDate.nameDay}${someDate.dateDay}`);
+  });
+});
